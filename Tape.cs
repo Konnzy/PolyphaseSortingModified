55843f49dfb68cdb9ad5adbaf25c4857880eb82d
Konnzy <dholodenko3@gmail.com>
2025-10-30 17:50:34 +0200

    Remove read/write methods from Tape class
    Retain Tape class structure while removing methods for reading and writing.

--- Tape.cs
+++ Tape.cs
@@ -7,33 +7,5 @@ namespace PolyphaseSorting
         private StreamWriter? writer;
 
         public Tape(string path) => Path = path;
-
-        public void OpenForWrite()
-        {
-            Close();
-            writer = new StreamWriter(new FileStream(Path, FileMode.Create, FileAccess.Write));
-        }
-
-        public void OpenForRead()
-        {
-            Close();
-            reader = new StreamReader(new FileStream(Path, FileMode.Open, FileAccess.Read));
-        }
-
-        public void WriteRecord(Record r) => writer?.WriteLine(r.ToString());
-
-        public Record? ReadRecord()
-        {
-            string? line = reader?.ReadLine();
-            return line == null ? null : Record.Parse(line);
-        }
-
-        public bool Eot => reader?.EndOfStream ?? true;
-
-        private void Close()
-        {
-            writer?.Close();
-            reader?.Close();
-        }
     }
 }
