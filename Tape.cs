969bf30aef02faddc12fbf95f0f49e373ca3af60
Konnzy <dholodenko3@gmail.com>
2025-10-30 17:55:39 +0200

    Update Tape.cs

--- Tape.cs
+++ Tape.cs
@@ -3,9 +3,8 @@ namespace PolyphaseSorting
     public class Tape
     {
         public string Path { get; }
-        private StreamReader? reader;
-        private StreamWriter? writer;
 
         public Tape(string path) => Path = path;
     }
 }
+
